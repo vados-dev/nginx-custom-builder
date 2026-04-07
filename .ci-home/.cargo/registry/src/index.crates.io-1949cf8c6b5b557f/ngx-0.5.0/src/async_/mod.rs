bb99b6879feb351cdf370099e83d69e20c1246cd
vados-dev <192440777+vados-dev@users.noreply.github.com>
2026-04-07 08:40:09 +0300

    chore: stop tracking .ci-home artifacts

--- .ci-home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ngx-0.5.0/src/async_/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Async runtime and set of utilities on top of the NGINX event loop.
-pub use self::sleep::{sleep, Sleep};
-pub use self::spawn::{spawn, Task};
-
-pub mod resolver;
-
-mod sleep;
-mod spawn;
