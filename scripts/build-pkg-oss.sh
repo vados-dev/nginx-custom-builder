270a74f6b611df91549aa64c849b459d8fd05c09
vados-dev <192440777+vados-dev@users.noreply.github.com>
2026-05-15 02:48:33 +0300

    Auto-commit: build-pkg-oss.sh updated on 5/15/2026 2026-05-14T23:48:33.905Z

--- scripts/build-pkg-oss.sh
+++ scripts/build-pkg-oss.sh
@@ -85,7 +85,7 @@ pushd "${work_root}/pkg-oss/${pkg_dir}" >/dev/null
 if [[ -n "${base_modules//[[:space:]]/}" ]]; then
   read -r -a modules_array <<< "${base_modules}"
   make_targets=()
-  
+  if [[ "${version_mode}" == "latest_tag" ]]; then
   for mod in "${modules_array[@]}"; do
     [[ -z "${mod}" ]] && continue
     make_targets+=("module-${mod}")
